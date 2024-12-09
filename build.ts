a616e1abf422f371450c360109bf5508419694a7
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-12-09 15:53:45 -0500

    build

---
+++ build.ts
@@ -0,0 +1,14 @@
+import esbuild from "esbuild";
+
+await esbuild.build({
+  entryPoints: ["./src/index.ts"],
+  bundle: true,
+  minify: false,
+  outdir: "./dist",
+  platform: "node",
+  format: "esm",
+  target: "esnext",
+  banner: {
+    js: 'import { createRequire } from "module";const require = createRequire(import.meta.url);',
+  },
+});
