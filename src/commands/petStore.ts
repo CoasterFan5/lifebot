43b5711c703b891ba210ea0cad06f13fac6fd086
WildkrattFan <cosmiccollectors131@gmail.com>
2024-12-09 21:06:43 -0600

    Petstore

--- src/commands/petStore.ts
+++ src/commands/petStore.ts
@@ -7,6 +7,7 @@ import { LifebotCommand } from '../types/commandTypes';
 import { db } from '../db';
 import { petsTable } from '../db/schema';
 import { Color } from "../utils/colors";
+import { eq, lt } from 'drizzle-orm';
 
 export const petstore: LifebotCommand = {
   command: new SlashCommandBuilder()
@@ -20,17 +21,24 @@ export const petstore: LifebotCommand = {
           petName: petsTable.petName,
           species: petsTable.species,
           age: petsTable.age,
-          price: petsTable.price
+          price: petsTable.price,
+          isSold: petsTable.isSold
         })
         .from(petsTable)
-        // .where(petsTable.isSold.eq(false))
+        .where(eq(petsTable.isSold, false))
         .limit(10)
         .execute();
       console.log(results);
       const embed = new EmbedBuilder()
         .setTitle("Pet Store")
         .setColor(Color.GREEN)
-        .setDescription(`Here are the pets available for sale: \n\n${results.map(pet => `ID: ${pet.id} ${pet.petName} (${pet.species}), Age: ${pet.age}, Price: ${pet.price}`).join('\n')}`);
+        
+        .setDescription(`Here are the pets available for sale: \n`)
+        .addFields(
+          {name: "Pet Name", value: results.map((result) => result.petName).join("\n"), inline: true},
+          {name: "Species", value: results.map((result) => result.species).join("\n"), inline: true},
+          {name: "Price", value: results.map((result) => result.price).join("\n"), inline: true},
+        )
 
       interaction.reply({
         embeds: [embed],
