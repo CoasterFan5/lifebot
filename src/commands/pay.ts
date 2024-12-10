474192b91d52d566f20538f609a24fa716c89d48
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-12-09 21:20:42 -0500

    pay command

--- src/commands/pay.ts
+++ src/commands/pay.ts
@@ -2,10 +2,15 @@ import {
   EmbedBuilder,
   SlashCommandBuilder,
   SlashCommandIntegerOption,
+  SlashCommandOptionsOnlyBuilder,
   SlashCommandUserOption,
 } from "discord.js";
 import { LifebotCommand } from "../types/commandTypes";
 import { Color } from "../utils/colors";
+import { db } from "../db";
+import { usersTable } from "../db/schema";
+import { eq, sql } from "drizzle-orm";
+import { getPersonalizedEmbed } from "../utils/personalizedEmbed";
 
 const errorEmbedTemplate = new EmbedBuilder()
   .setTitle("Uh Oh!")
@@ -28,6 +33,72 @@ export const pay: LifebotCommand = {
         .setRequired(true),
     ),
   handler: async (interaction, user) => {
-    if((user.balance || 0) < interaction.options.get(""))
+    if (!interaction.isChatInputCommand()) {
+      return;
+    }
+
+    const payAmount = interaction.options.getInteger("amount", true);
+    const otherDiscordUser = interaction.options.getUser("user", true);
+
+    console.log();
+
+    if (payAmount < 0) {
+      await db
+        .update(usersTable)
+        .set({
+          balance: sql`${usersTable.balance} - 10`,
+        })
+        .where(eq(usersTable.userId, user.userId));
+
+      interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setColor(Color.RED)
+            .setTitle("Nice try")
+            .setDescription(
+              `I have fined you $10 for trying to steal money from <@${otherDiscordUser.id}>`,
+            ),
+        ],
+      });
+
+      return;
+    }
+
+    if ((user.balance || 0) < payAmount) {
+      interaction.reply({
+        embeds: [
+          errorEmbedTemplate.setDescription("You can't afford this. Brokie."),
+        ],
+      });
+      return;
+    }
+    await db
+      .insert(usersTable)
+      .values({
+        userId: otherDiscordUser.id,
+        balance: payAmount,
+      })
+      .onConflictDoUpdate({
+        target: usersTable.userId,
+        set: {
+          userId: otherDiscordUser.id,
+          balance: sql`${usersTable.balance} + ${payAmount}`,
+        },
+      });
+
+    await db
+      .update(usersTable)
+      .set({
+        balance: (user.balance || 0) - payAmount,
+      })
+      .where(eq(usersTable.userId, user.userId));
+
+    interaction.reply({
+      embeds: [
+        getPersonalizedEmbed(interaction.user).setDescription(
+          `You sent $${payAmount} to <@${otherDiscordUser.id}>`,
+        ),
+      ],
+    });
   },
 };
