d084b9d89132381e6a54f913461ec92a15eef629
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-12-11 16:25:55 -0500

    add other users to rank options

--- src/commands/rank.ts
+++ src/commands/rank.ts
@@ -1,14 +1,37 @@
-import { count, gt } from "drizzle-orm";
+import { count, eq, gt } from "drizzle-orm";
 import { db } from "../db";
 import { usersTable } from "../db/schema";
 import type { LifebotCommand } from "../types/commandTypes";
-import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import {
+  EmbedBuilder,
+  SlashCommandBuilder,
+  SlashCommandUserOption,
+} from "discord.js";
+import { Color } from "../utils/colors";
 
 export const rank: LifebotCommand = {
   command: new SlashCommandBuilder()
     .setName("rank")
-    .setDescription("View global ranking."),
+    .setDescription("View global ranking.")
+    .addUserOption(
+      new SlashCommandUserOption()
+        .setName("user")
+        .setDescription("See another user's rank"),
+    ),
   handler: async (interaction, user, client) => {
+    const otherUser = interaction.options.getUser("user", false);
+    let userToCheck = user;
+
+    if (otherUser) {
+      const dbUser = await db
+        .select()
+        .from(usersTable)
+        .where(eq(usersTable.userId, otherUser.id));
+      if (dbUser[0]) {
+        userToCheck = dbUser[0];
+      }
+    }
+
     const totalUsers = await db
       .select({
         count: count(),
@@ -20,15 +43,27 @@ export const rank: LifebotCommand = {
         count: count(),
       })
       .from(usersTable)
-      .where(gt(usersTable.balance, user.balance || 0));
+      .where(gt(usersTable.balance, userToCheck.balance || 0));
 
+    const rankString = `${aboveThisUser[0].count + 1}/${totalUsers[0].count}`;
+
+    if (otherUser) {
+      interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setTitle(`${otherUser.displayName}'s rank`)
+            .setDescription(`${otherUser.displayName} is ranked ${rankString}`)
+            .setColor(Color.GREEN),
+        ],
+      });
+      return;
+    }
     interaction.reply({
       embeds: [
         new EmbedBuilder()
           .setTitle("Rank")
-          .setDescription(
-            `You are rank ${aboveThisUser[0].count + 1}/${totalUsers[0].count}`,
-          ),
+          .setDescription(`You are ranked ${rankString}`)
+          .setColor(Color.GREEN),
       ],
     });
   },
--- src/types/commandTypes.ts
+++ src/types/commandTypes.ts
@@ -1,23 +1,20 @@
 import {
-	type CacheType,
-	type ChatInputCommandInteraction,
-	type Client,
-	type MessageContextMenuCommandInteraction,
-	type SharedSlashCommand,
-	SlashCommandBuilder,
-	type UserContextMenuCommandInteraction,
+  type CacheType,
+  type ChatInputCommandInteraction,
+  type Client,
+  type MessageContextMenuCommandInteraction,
+  type SharedSlashCommand,
+  SlashCommandBuilder,
+  type UserContextMenuCommandInteraction,
 } from "discord.js";
 
 import type { usersTable } from "../db/schema";
 
 export type LifebotCommand = {
-	command: SharedSlashCommand;
-	handler: (
-		interaction:
-			| ChatInputCommandInteraction<CacheType>
-			| MessageContextMenuCommandInteraction<CacheType>
-			| UserContextMenuCommandInteraction<CacheType>,
-		user: typeof usersTable.$inferSelect,
-		client: Client<boolean>,
-	) => Promise<void>;
+  command: SharedSlashCommand;
+  handler: (
+    interaction: ChatInputCommandInteraction<CacheType>,
+    user: typeof usersTable.$inferSelect,
+    client: Client<boolean>,
+  ) => Promise<void>;
 };
