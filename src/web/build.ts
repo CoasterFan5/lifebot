c5074751a2294bca84a0ae9bb9f7430f43b97f93
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-12-16 18:09:05 -0500

    format

--- src/web/build.ts
+++ src/web/build.ts
@@ -1,14 +1,14 @@
 import esbuild from "esbuild";
 
 await esbuild.build({
-  entryPoints: ["./src/web/index.ts"],
-  bundle: true,
-  minify: false,
-  outdir: "./dist/web",
-  platform: "node",
-  format: "esm",
-  target: "esnext",
-  banner: {
-    js: 'import { createRequire } from "module";const require = createRequire(import.meta.url);',
-  },
+	entryPoints: ["./src/web/index.ts"],
+	bundle: true,
+	minify: false,
+	outdir: "./dist/web",
+	platform: "node",
+	format: "esm",
+	target: "esnext",
+	banner: {
+		js: 'import { createRequire } from "module";const require = createRequire(import.meta.url);',
+	},
 });
