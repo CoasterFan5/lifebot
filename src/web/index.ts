e6d9daa796bfefaeecd19627334aa17c2a29a38d
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-12-17 10:29:56 -0500

    update fastify host

--- src/web/index.ts
+++ src/web/index.ts
@@ -1,9 +1,9 @@
 import { eq } from "drizzle-orm";
-import Fastify from "fastify";
+import Fastify, { fastify } from "fastify";
 import {
-	type ZodTypeProvider,
-	serializerCompiler,
-	validatorCompiler,
+  type ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
 } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { db } from "../db";
@@ -11,42 +11,50 @@ import { usersTable } from "../db/schema";
 
 const api_key = process.env.API_KEY;
 const app = Fastify({
-	logger: true,
+  logger: true,
 });
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.withTypeProvider<ZodTypeProvider>().route({
-	method: "GET",
-	url: "/api/v1/:key/user/:userId",
-	// Define your schema
-	schema: {
-		params: z.object({
-			key: z.string(),
-			userId: z.string(),
-		}),
-	},
-	preHandler: async (req, res) => {
-		if (req.params.key !== api_key) {
-			res.code(401).send({ message: "Invalid Key" });
-		}
-	},
-	handler: async (req, res) => {
-		const { userId } = req.params;
-		const user = await db
-			.select()
-			.from(usersTable)
-			.where(eq(usersTable.userId, userId));
+  method: "GET",
+  url: "/api/v1/:key/user/:userId",
+  // Define your schema
+  schema: {
+    params: z.object({
+      key: z.string(),
+      userId: z.string(),
+    }),
+  },
+  preHandler: async (req, res) => {
+    if (req.params.key !== api_key) {
+      res.code(401).send({ message: "Invalid Key" });
+    }
+  },
+  handler: async (req, res) => {
+    const { userId } = req.params;
+    const user = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.userId, userId));
 
-		if (user.length < 1) {
-			return res.code(401).send({ message: "No user" });
-		}
+    if (user.length < 1) {
+      return res.code(401).send({ message: "No user" });
+    }
 
-		res.send(user[0]);
-	},
+    res.send(user[0]);
+  },
 });
 
-app.listen({
-	port: Number.parseInt(process.env.PORT || "3000"),
-});
+app.listen(
+  {
+    host: "::",
+    port: Number.parseInt(process.env.PORT || "3000"),
+  },
+  (err, address) => {
+    if (err) {
+      console.error(err);
+    }
+  },
+);
