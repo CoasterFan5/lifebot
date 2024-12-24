52f0f0eac65b9c47e31619bf90390ac11886c896
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-12-24 15:28:43 -0500

    actually add skills command

---
+++ src/bot/commands/skills.ts
@@ -0,0 +1,29 @@
+import { SlashCommandBuilder } from "discord.js";
+import type { LifebotCommand } from "../types/commandTypes";
+import { getPersonalizedEmbed } from "../utils/personalizedEmbed";
+
+export const skills: LifebotCommand = {
+  command: new SlashCommandBuilder()
+    .setName("skills")
+    .setDescription("Check skill xp"),
+  handler: async (interaction, user, client) => {
+    const skillsEmbed = getPersonalizedEmbed(interaction.user);
+
+    skillsEmbed.setDescription(`
+Technical Skills: ${user.technicalSkills}xp
+Creativity: ${user.creativity}xp
+Customer Service: ${user.customerService}xp
+Organization: ${user.organization}xp
+Leadership: ${user.leadership}xp
+Time Management: ${user.timeManagement}xp
+Teamwork: ${user.teamwork}xp
+Work Ethic: ${user.workEthic}xp
+Criminality: ${user.creativity}xp
+Reputation: ${user.reputation}xp
+`);
+
+    interaction.reply({
+      embeds: [skillsEmbed],
+    });
+  },
+};
