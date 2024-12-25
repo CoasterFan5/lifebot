c344d55e0c32765d5ec71c33420c962d9373d114
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-12-25 15:42:08 -0500

    wrap jobs update

--- src/bot/commands/job/info.ts
+++ src/bot/commands/job/info.ts
@@ -3,47 +3,58 @@ import type { LifebotCommandHandler } from "../../types/commandTypes";
 import { Color } from "../../utils/colors";
 import { getPersonalizedEmbed } from "../../utils/personalizedEmbed";
 import { jobPaths } from "./jobList";
+import type { StringedObject } from "../../types/stringedObject";
 
 const noJobEmbed = new EmbedBuilder()
-	.setTitle("You don't have a job.")
-	.setDescription("Get your money up.")
-	.setColor(Color.RED)
-	.setFooter({
-		text: "Try /job search",
-	});
+  .setTitle("You don't have a job.")
+  .setDescription("Get your money up.")
+  .setColor(Color.RED)
+  .setFooter({
+    text: "Try /job search",
+  });
 
 export const info: LifebotCommandHandler = async (
-	interaction,
-	user,
-	client,
+  interaction,
+  user,
+  client,
 ) => {
-	if (!user.hasJob) {
-		interaction.reply({
-			embeds: [noJobEmbed],
-		});
-		return;
-	}
+  if (!user.hasJob) {
+    interaction.reply({
+      embeds: [noJobEmbed],
+    });
+    return;
+  }
 
-	const isThereANextTier =
-		jobPaths[user.jobPath].tiers.length > user.jobTierIndex + 1;
+  const isThereANextTier =
+    jobPaths[user.jobPath].tiers.length > user.jobTierIndex + 1;
 
-	const reputationRequired = isThereANextTier
-		? jobPaths[user.jobPath].tiers[user.jobTierIndex + 1].requirements
-				.reputation
-		: "Infinity";
+  const jobDetails = jobPaths[user.jobPath].tiers[user.jobTierIndex];
+  const nextJobDetails = jobPaths[user.jobPath].tiers[user.jobTierIndex + 1];
 
-	const jobInfoEmbed = getPersonalizedEmbed(interaction.user);
-	jobInfoEmbed.addFields({
-		name: "Job Info",
-		value: [
-			`**${user.jobName}** at ${user.jobCompany}`,
-			`Pay range: $${user.jobMinPay}-$${user.jobMaxPay}`,
-			`Job Tier: ${user.jobTierIndex + 1}/${jobPaths[user.jobPath].tiers.length}`,
-			`Reputation Progress: ${user.reputation}/${reputationRequired}`,
-		].join("\n"),
-	});
+  const jobSkillsInfo = [];
+  for (const key in jobDetails.xpGrants) {
+    jobSkillsInfo.push(
+      `${key}: ${(user as unknown as StringedObject<number>)[key]}/${(nextJobDetails.requirements as StringedObject<number>)[key] || "0"} | +${(jobDetails.xpGrants as StringedObject<number>)[key]}`,
+    );
+  }
 
-	interaction.reply({
-		embeds: [jobInfoEmbed],
-	});
+  const jobInfoEmbed = getPersonalizedEmbed(interaction.user);
+  jobInfoEmbed.addFields(
+    {
+      name: "Job Info",
+      value: [
+        `**${user.jobName}** at ${user.jobCompany}`,
+        `Pay range: $${user.jobMinPay}-$${user.jobMaxPay}`,
+        `Job Tier: ${user.jobTierIndex + 1}/${jobPaths[user.jobPath].tiers.length}`,
+      ].join("\n"),
+    },
+    {
+      name: "Skill Info",
+      value: jobSkillsInfo.join("\n"),
+    },
+  );
+
+  interaction.reply({
+    embeds: [jobInfoEmbed],
+  });
 };
--- src/bot/commands/work.ts
+++ src/bot/commands/work.ts
@@ -5,52 +5,59 @@ import { usersTable } from "../../db/schema";
 import { workMessages } from "../textBank/work";
 import type { LifebotCommand } from "../types/commandTypes";
 import { Color } from "../utils/colors";
+import { jobWork } from "./job/jobWork";
+import { client } from "..";
 
 const WORK_COOLDOWN = 60_000; // ms in work cooldown
 
 export const work: LifebotCommand = {
-	command: new SlashCommandBuilder()
-		.setName("work")
-		.setDescription("Do a job to earn money!"),
-	handler: async (interaction, user) => {
-		const currentTime = Date.now();
-		const msSinceLastWork = currentTime - (user.lastWork?.getTime() || 0);
-		if (msSinceLastWork < WORK_COOLDOWN) {
-			const timeToWork = Math.ceil((WORK_COOLDOWN - msSinceLastWork) / 1000);
+  command: new SlashCommandBuilder()
+    .setName("work")
+    .setDescription("Do a job to earn money!"),
+  handler: async (interaction, user) => {
+    const currentTime = Date.now();
+    const msSinceLastWork = currentTime - (user.lastWork?.getTime() || 0);
+    if (msSinceLastWork < WORK_COOLDOWN) {
+      const timeToWork = Math.ceil((WORK_COOLDOWN - msSinceLastWork) / 1000);
 
-			await interaction.reply({
-				embeds: [
-					new EmbedBuilder()
-						.setColor(Color.RED)
-						.setTitle("Calm down!")
-						.setDescription(`You can work again in ${timeToWork} seconds`),
-				],
-			});
-			return;
-		}
+      await interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setColor(Color.RED)
+            .setTitle("Calm down!")
+            .setDescription(`You can work again in ${timeToWork} seconds`),
+        ],
+      });
+      return;
+    }
 
-		const amount = Math.floor(Math.random() * 900) + 100;
-		await db
-			.update(usersTable)
-			.set({ balance: amount + (user.balance || 0), lastWork: new Date() })
-			.where(eq(usersTable.userId, user.userId));
+    if (user.hasJob) {
+      await jobWork(interaction, user, client);
+      return;
+    }
 
-		const messageIndex = Math.floor(Math.random() * workMessages.length);
-		const message = workMessages[messageIndex].replaceAll("{}", `$${amount}`);
+    const amount = Math.floor(Math.random() * 900) + 100;
+    await db
+      .update(usersTable)
+      .set({ balance: amount + (user.balance || 0), lastWork: new Date() })
+      .where(eq(usersTable.userId, user.userId));
 
-		await interaction.reply({
-			embeds: [
-				new EmbedBuilder()
-					.setColor(Color.GREEN)
-					.setDescription(message)
-					.setAuthor({
-						name: interaction.user.displayName,
-						iconURL: interaction.user.displayAvatarURL(),
-					})
-					.setFooter({
-						text: `Mid: ${messageIndex}`,
-					}),
-			],
-		});
-	},
+    const messageIndex = Math.floor(Math.random() * workMessages.length);
+    const message = workMessages[messageIndex].replaceAll("{}", `$${amount}`);
+
+    await interaction.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setColor(Color.GREEN)
+          .setDescription(message)
+          .setAuthor({
+            name: interaction.user.displayName,
+            iconURL: interaction.user.displayAvatarURL(),
+          })
+          .setFooter({
+            text: `Mid: ${messageIndex}`,
+          }),
+      ],
+    });
+  },
 };
