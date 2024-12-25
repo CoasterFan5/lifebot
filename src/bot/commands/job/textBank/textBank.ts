094c0ecd8d2b5dcd24f66ee174118d4954829d21
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-12-25 15:58:11 -0500

    format

--- src/bot/commands/job/textBank/textBank.ts
+++ src/bot/commands/job/textBank/textBank.ts
@@ -1,9 +1,9 @@
 export const workMessages = [
-  "I can't believe they only pay you ${amount} for being such a great {jobTitle}",
-  "{company} should pay their employees more than ${amount}",
-  "Congrats on being a {jobTitle}, enjoy your ${amount}",
-  "You suck at being a {jobTitle}, I can't believe they pay you ${amount}",
-  "You could be making money irl, but instead you decide to grind a discord bot. +${amount}",
-  "I just care so much about your {jobTitle} position. Here is ${amount}. What else do you want? A participation trophy?",
-  "The whole {jobTitle} title really suits you. +${amount}",
+	"I can't believe they only pay you ${amount} for being such a great {jobTitle}",
+	"{company} should pay their employees more than ${amount}",
+	"Congrats on being a {jobTitle}, enjoy your ${amount}",
+	"You suck at being a {jobTitle}, I can't believe they pay you ${amount}",
+	"You could be making money irl, but instead you decide to grind a discord bot. +${amount}",
+	"I just care so much about your {jobTitle} position. Here is ${amount}. What else do you want? A participation trophy?",
+	"The whole {jobTitle} title really suits you. +${amount}",
 ];
