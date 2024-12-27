4594647f44056e8bc26d1a58f965b0af179c8998
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-12-27 18:55:55 -0500

    add support command

---
+++ src/bot/commands/support.ts
@@ -0,0 +1,19 @@
+import { SlashCommandBuilder } from "discord.js";
+import type { LifebotCommand } from "../types/commandTypes";
+import { getPersonalizedEmbed } from "../utils/personalizedEmbed";
+
+export const support: LifebotCommand = {
+	command: new SlashCommandBuilder()
+		.setName("support")
+		.setDescription("Support server link"),
+	handler: async (interaction, user, client) => {
+		const embed = getPersonalizedEmbed(interaction.user);
+		embed.setDescription(
+			"I am here to help! Just join the [support server](https://discord.gg/BDF3efd8KM)",
+		);
+		interaction.reply({
+			embeds: [embed],
+		});
+		return;
+	},
+};
