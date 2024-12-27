a49e7a21d686bc82dd39be12e53df29ebf6607b9
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-12-27 18:35:08 -0500

    update anti-bot system with dynamic messages

--- src/bot/commands/job/jobWork.ts
+++ src/bot/commands/job/jobWork.ts
@@ -1,8 +1,8 @@
 import {
-	ActionRowBuilder,
-	ButtonBuilder,
-	ButtonStyle,
-	EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  EmbedBuilder,
 } from "discord.js";
 import { eq } from "drizzle-orm";
 import { db } from "../../../db";
@@ -16,152 +16,154 @@ import { checkForPromotion } from "./jobUtils/checkForPromotion";
 import { workMessages } from "./textBank/textBank";
 
 export const jobWork: LifebotCommandHandler = async (
-	interaction,
-	user,
-	client,
+  interaction,
+  user,
+  client,
 ) => {
-	const userJobInfo = jobPaths[user.jobPath].tiers[user.jobTierIndex];
+  const userJobInfo = jobPaths[user.jobPath].tiers[user.jobTierIndex];
 
-	const min = userJobInfo.basePay;
-	const max = userJobInfo.maxPay;
+  const min = userJobInfo.basePay;
+  const max = userJobInfo.maxPay;
 
-	const thisPay = Math.floor(Math.random() * (max - min)) + min;
+  const thisPay = Math.floor(Math.random() * (max - min)) + min;
 
-	const message = workMessages[Math.floor(Math.random() * workMessages.length)];
-	const fullMessage = message
-		.replaceAll("{jobTitle}", user.jobName.toLowerCase())
-		.replaceAll("{amount}", thisPay.toString())
-		.replaceAll("{company}", user.jobCompany);
+  const message = workMessages[Math.floor(Math.random() * workMessages.length)];
+  const fullMessage = message
+    .replaceAll("{jobTitle}", user.jobName.toLowerCase())
+    .replaceAll("{amount}", thisPay.toString())
+    .replaceAll("{company}", user.jobCompany);
 
-	const xpGrants = userJobInfo.xpGrants;
+  const xpGrants = userJobInfo.xpGrants;
 
-	// This is a lot, but it just updates the balance, last work, and xp
+  // This is a lot, but it just updates the balance, last work, and xp
 
-	const newUser = await db
-		.update(usersTable)
-		.set({
-			balance: thisPay + (user.balance || 0),
-			lastWork: new Date(),
-			technicalSkills: increment(
-				usersTable.technicalSkills,
-				xpGrants.technicalSkills || 0,
-			),
-			creativity: increment(usersTable.creativity, xpGrants.creativity || 0),
-			customerService: increment(
-				usersTable.customerService,
-				xpGrants.customerService || 0,
-			),
-			organization: increment(
-				usersTable.organization,
-				xpGrants.organization || 0,
-			),
-			leadership: increment(usersTable.leadership, xpGrants.leadership || 0),
-			timeManagement: increment(
-				usersTable.timeManagement,
-				xpGrants.timeManagement || 0,
-			),
-			teamwork: increment(usersTable.teamwork, xpGrants.teamwork || 0),
-			workEthic: increment(usersTable.workEthic, xpGrants.workEthic || 0),
-			criminality: increment(usersTable.criminality, xpGrants.criminality || 0),
-			reputation: increment(usersTable.reputation, xpGrants.reputation || 0),
-		})
-		.where(eq(usersTable.userId, user.userId))
-		.returning();
+  const newUser = await db
+    .update(usersTable)
+    .set({
+      balance: thisPay + (user.balance || 0),
+      lastWork: new Date(),
+      technicalSkills: increment(
+        usersTable.technicalSkills,
+        xpGrants.technicalSkills || 0,
+      ),
+      creativity: increment(usersTable.creativity, xpGrants.creativity || 0),
+      customerService: increment(
+        usersTable.customerService,
+        xpGrants.customerService || 0,
+      ),
+      organization: increment(
+        usersTable.organization,
+        xpGrants.organization || 0,
+      ),
+      leadership: increment(usersTable.leadership, xpGrants.leadership || 0),
+      timeManagement: increment(
+        usersTable.timeManagement,
+        xpGrants.timeManagement || 0,
+      ),
+      teamwork: increment(usersTable.teamwork, xpGrants.teamwork || 0),
+      workEthic: increment(usersTable.workEthic, xpGrants.workEthic || 0),
+      criminality: increment(usersTable.criminality, xpGrants.criminality || 0),
+      reputation: increment(usersTable.reputation, xpGrants.reputation || 0),
+    })
+    .where(eq(usersTable.userId, user.userId))
+    .returning();
 
-	const embedList: EmbedBuilder[] = [];
+  const embedList: EmbedBuilder[] = [];
 
-	const embed = getPersonalizedEmbed(interaction.user);
-	embed.setDescription(fullMessage);
-	embedList.push(embed);
+  const embed = getPersonalizedEmbed(interaction.user);
+  embed.setDescription(fullMessage);
+  embedList.push(embed);
 
-	if (await checkForPromotion(newUser[0])) {
-		embedList.push(
-			new EmbedBuilder()
-				.setTitle("Promotion Time!")
-				.setDescription(
-					"You have been promoted! Use `/job info` to check your new job details!",
-				)
-				.setColor(Color.GOLD),
-		);
-	}
+  if (await checkForPromotion(newUser[0])) {
+    embedList.push(
+      new EmbedBuilder()
+        .setTitle("Promotion Time!")
+        .setDescription(
+          "You have been promoted! Use `/job info` to check your new job details!",
+        )
+        .setColor(Color.GOLD),
+    );
+  }
 
-	const buttons = new ActionRowBuilder<ButtonBuilder>();
+  // Anti-bot code
+  const checkAntiBot = Math.random() < 1 / 15;
 
-	// Anti-bot code
-	const checkAntiBot = Math.random() < 10;
+  if (!checkAntiBot) {
+    //just reply with what we have
+    await interaction.reply({
+      embeds: embedList,
+    });
+    return;
+  }
 
-	if (!checkAntiBot) {
-		//just reply with what we have
-		await interaction.reply({
-			embeds: embedList,
-			components: [buttons],
-		});
-	}
+  const buttons = new ActionRowBuilder<ButtonBuilder>();
 
-	buttons.addComponents(
-		new ButtonBuilder()
-			.setLabel("Join meeting!")
-			.setStyle(ButtonStyle.Danger)
-			.setCustomId("joinMeeting"),
-	);
+  buttons.addComponents(
+    new ButtonBuilder()
+      .setLabel("Join meeting!")
+      .setStyle(ButtonStyle.Danger)
+      .setCustomId("joinMeeting"),
+  );
 
-	embedList.push(
-		new EmbedBuilder()
-			.setTitle("Emergency Meeting!")
-			.setDescription(
-				`${user.jobCompany} has called an all-hands on deck meeting. If you do not attend by click the button attatched to this message, you will lose your job and be fined 30% of your balance.`,
-			)
-			.setFooter({
-				text: "You have 60 seconds\nIf a technical bug results in failing this challenge, join our support server.",
-			})
-			.setColor(Color.ORANGE),
-	);
+  const emergencyMeetingText = jobPaths[
+    user.jobPath
+  ].emergencyMeetingText.replaceAll("{companyName}", user.jobCompany);
 
-	const sent = await interaction.reply({
-		embeds: embedList,
-		components: [buttons],
-	});
+  embedList.push(
+    new EmbedBuilder()
+      .setTitle("Emergency Meeting!")
+      .setDescription(emergencyMeetingText)
+      .setFooter({
+        text: "You have 90 seconds to click the button below.\nIf a technical bug results in failing this challenge, join our support server.",
+      })
+      .setColor(Color.ORANGE),
+  );
 
-	sent
-		.awaitMessageComponent({
-			filter: (interaction) => interaction.user.id === user.userId,
-			time: 5_000,
-		})
-		.then((newInteraction) => {
-			newInteraction.reply({
-				embeds: [
-					new EmbedBuilder()
-						.setTitle("That went well!")
-						.setDescription("Seems like it was a very productive meeting.")
-						.setColor(Color.GREEN),
-				],
-			});
-		})
-		.catch(async (e) => {
-			const lostMoney = Math.ceil((user.balance || 0) * 0.3);
+  const sent = await interaction.reply({
+    embeds: embedList,
+    components: [buttons],
+  });
 
-			embedList.push(
-				new EmbedBuilder()
-					.setTitle("Um, you didn't go to the meeting")
-					.setDescription("This is awkward.")
-					.addFields({
-						name: "Punishments",
-						value: `-$${lostMoney}\nFired from job\nLaughed at`,
-					})
-					.setColor(Color.ORANGE),
-			);
+  sent
+    .awaitMessageComponent({
+      filter: (interaction) => interaction.user.id === user.userId,
+      time: 90_000,
+    })
+    .then((newInteraction) => {
+      newInteraction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setTitle("That went well!")
+            .setDescription("Seems like it was a very productive meeting.")
+            .setColor(Color.GREEN),
+        ],
+      });
+    })
+    .catch(async (e) => {
+      const lostMoney = Math.ceil((user.balance || 0) * 0.3);
 
-			await db
-				.update(usersTable)
-				.set({
-					balance: increment(usersTable.balance, -lostMoney),
-					hasJob: false,
-				})
-				.where(eq(usersTable.userId, user.userId));
+      embedList.push(
+        new EmbedBuilder()
+          .setTitle("Um, you didn't go to the meeting")
+          .setDescription("This is awkward.")
+          .addFields({
+            name: "Punishments",
+            value: `-$${lostMoney}\nFired from job\nLaughed at`,
+          })
+          .setColor(Color.ORANGE),
+      );
 
-			interaction.editReply({
-				embeds: embedList,
-				components: [],
-			});
-		});
+      await db
+        .update(usersTable)
+        .set({
+          balance: increment(usersTable.balance, -lostMoney),
+          hasJob: false,
+        })
+        .where(eq(usersTable.userId, user.userId));
+
+      interaction.editReply({
+        embeds: embedList,
+        components: [],
+      });
+    });
 };
--- src/bot/types/jobs.ts
+++ src/bot/types/jobs.ts
@@ -1,33 +1,34 @@
 const skillList = [
-	"technicalSkills",
-	"creativity",
-	"customerService",
-	"organization",
-	"leadership",
-	"timeManagement",
-	"teamwork",
-	"workEthic",
-	"criminality",
-	"reputation",
-	"nepotism",
+  "technicalSkills",
+  "creativity",
+  "customerService",
+  "organization",
+  "leadership",
+  "timeManagement",
+  "teamwork",
+  "workEthic",
+  "criminality",
+  "reputation",
+  "nepotism",
 ] as const;
 
 type SkillName = (typeof skillList)[number];
 export type SkillValues = Partial<Record<SkillName, number>>;
 
 export type JobTier = {
-	title: string;
-	basePay: number;
-	maxPay: number;
-	requirements: SkillValues;
-	xpGrants: SkillValues;
+  title: string;
+  basePay: number;
+  maxPay: number;
+  requirements: SkillValues;
+  xpGrants: SkillValues;
 };
 
 export type JobDetails = {
-	companies: string[];
-	tiers: JobTier[];
+  companies: string[];
+  emergencyMeetingText: string;
+  tiers: JobTier[];
 };
 
 export type JobList = {
-	[key: string]: JobDetails;
+  [key: string]: JobDetails;
 };
