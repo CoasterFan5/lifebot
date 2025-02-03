b4b57df939790d832735c33e2b2995cc69e39abe
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-02-03 11:53:17 -0500

    forgot

---
+++ drizzle/0010_dear_silver_sable.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS "houses" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"ownerId" varchar(256) NOT NULL,
+	"location" integer NOT NULL,
+	"quality" integer NOT NULL,
+	"squareFootage" integer NOT NULL,
+	"furnitureScore" integer NOT NULL,
+	"leased" boolean DEFAULT false NOT NULL,
+	"lastRentCollection" timestamp,
+	"rentPrice" integer DEFAULT 0 NOT NULL,
+	"tenantScore" integer DEFAULT 0 NOT NULL,
+	"tenanteWealth" integer DEFAULT 0 NOT NULL,
+	CONSTRAINT "houses_id_unique" UNIQUE("id")
+);
