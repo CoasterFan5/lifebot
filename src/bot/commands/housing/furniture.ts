1ffde8b4f82d8b351854821f7125be03d7446833
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-03-12 12:09:51 -0400

    format

--- src/bot/commands/housing/furniture.ts
+++ src/bot/commands/housing/furniture.ts
@@ -1,11 +1,11 @@
 import {
-  SlashCommandBuilder,
-  SlashCommandIntegerOption,
-  SlashCommandSubcommandBuilder,
+	SlashCommandBuilder,
+	SlashCommandIntegerOption,
+	SlashCommandSubcommandBuilder,
 } from "discord.js";
 import type {
-  LifebotCommand,
-  LifebotCommandHandler,
+	LifebotCommand,
+	LifebotCommandHandler,
 } from "../../types/commandTypes";
 import { buy } from "./furniture/buy";
 import { info } from "./furniture/info";
@@ -13,72 +13,72 @@ import { reclaim } from "./furniture/reclaim";
 import { sell } from "./furniture/sell";
 
 const subCommandHandlers: {
-  [key: string]: LifebotCommandHandler;
+	[key: string]: LifebotCommandHandler;
 } = {
-  buy,
-  info,
-  sell,
-  reclaim,
+	buy,
+	info,
+	sell,
+	reclaim,
 };
 
 export const furniture: LifebotCommand = {
-  command: new SlashCommandBuilder()
-    .setName("furniture")
-    .setDescription("Furniture")
-    .addSubcommand(
-      new SlashCommandSubcommandBuilder()
-        .setName("buy")
-        .setDescription("Buy a new piece of furniture"),
-    )
-    .addSubcommand(
-      new SlashCommandSubcommandBuilder()
-        .setName("sell")
-        .setDescription("Sell a piece of furniture")
-        .addIntegerOption(
-          new SlashCommandIntegerOption()
-            .setName("id")
-            .setDescription("Id of the furniture")
-            .setRequired(true),
-        ),
-    )
-    .addSubcommand(
-      new SlashCommandSubcommandBuilder()
-        .setName("reclaim")
-        .setDescription("Remove a piece of furniture from a house")
-        .addIntegerOption(
-          new SlashCommandIntegerOption()
-            .setName("id")
-            .setDescription("Id of the furniture")
-            .setRequired(true),
-        ),
-    )
-    .addSubcommand(
-      new SlashCommandSubcommandBuilder()
-        .setName("info")
-        .setDescription("View a piece of furniture you have")
-        .addIntegerOption(
-          new SlashCommandIntegerOption()
-            .setName("id")
-            .setDescription("Id of the furniture")
-            .setRequired(false),
-        )
-        .addIntegerOption(
-          new SlashCommandIntegerOption()
-            .setName("page")
-            .setDescription("The info page to view")
-            .setRequired(false),
-        ),
-    ),
-  handler: async (params) => {
-    try {
-      await subCommandHandlers[params.interaction.options.getSubcommand(true)](
-        params,
-      );
-    } catch (e) {
-      console.error(e);
-      params.interaction.reply(
-        "Error executing this command, try again later.",
-      );
-    }
-  },
+	command: new SlashCommandBuilder()
+		.setName("furniture")
+		.setDescription("Furniture")
+		.addSubcommand(
+			new SlashCommandSubcommandBuilder()
+				.setName("buy")
+				.setDescription("Buy a new piece of furniture"),
+		)
+		.addSubcommand(
+			new SlashCommandSubcommandBuilder()
+				.setName("sell")
+				.setDescription("Sell a piece of furniture")
+				.addIntegerOption(
+					new SlashCommandIntegerOption()
+						.setName("id")
+						.setDescription("Id of the furniture")
+						.setRequired(true),
+				),
+		)
+		.addSubcommand(
+			new SlashCommandSubcommandBuilder()
+				.setName("reclaim")
+				.setDescription("Remove a piece of furniture from a house")
+				.addIntegerOption(
+					new SlashCommandIntegerOption()
+						.setName("id")
+						.setDescription("Id of the furniture")
+						.setRequired(true),
+				),
+		)
+		.addSubcommand(
+			new SlashCommandSubcommandBuilder()
+				.setName("info")
+				.setDescription("View a piece of furniture you have")
+				.addIntegerOption(
+					new SlashCommandIntegerOption()
+						.setName("id")
+						.setDescription("Id of the furniture")
+						.setRequired(false),
+				)
+				.addIntegerOption(
+					new SlashCommandIntegerOption()
+						.setName("page")
+						.setDescription("The info page to view")
+						.setRequired(false),
+				),
+		),
+	handler: async (params) => {
+		try {
+			await subCommandHandlers[params.interaction.options.getSubcommand(true)](
+				params,
+			);
+		} catch (e) {
+			console.error(e);
+			params.interaction.reply(
+				"Error executing this command, try again later.",
+			);
+		}
+	},
 };
