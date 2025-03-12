2996408c71397170eaef4614dcc820464ef4dc24
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-03-12 12:13:21 -0400

    typos

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,13 +1,13 @@
 import {
-	ActivityType,
-	BitField,
-	Client,
-	EmbedBuilder,
-	Events,
-	GatewayIntentBits,
-	IntentsBitField,
-	InteractionType,
-	Poll,
+  ActivityType,
+  BitField,
+  Client,
+  EmbedBuilder,
+  Events,
+  GatewayIntentBits,
+  IntentsBitField,
+  InteractionType,
+  Poll,
 } from "discord.js";
 import { eq } from "drizzle-orm";
 import { db } from "../db";
@@ -20,101 +20,105 @@ const token = process.env.TOKEN;
 const botDisabled = process.env.DISABLE_BOT?.toLowerCase() === "true";
 
 const disabledEmbed = new EmbedBuilder()
-	.setColor(Color.GREEN)
-	.setTitle("We'll be right back!")
-	.setDescription(
-		"Life bot seems to be disabled right now. This is not good. If you must know whats going on, join the [support server](https://discord.gg/J5esKPaANg)",
-	);
+  .setColor(Color.GREEN)
+  .setTitle("We'll be right back!")
+  .setDescription(
+    "Life bot seems to be disabled right now. This is not good. If you must know whats going on, join the [support server](https://discord.gg/J5esKPaANg)",
+  );
 
 const client = new Client({
-	intents: [
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.MessageContent,
-	],
+  intents: [
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
 // Quick time events
 client.on("messageCreate", async (message) => {
-	if (botDisabled) {
-		return;
-	}
+  if (botDisabled) {
+    return;
+  }
 
-	if (Math.floor(Math.random() * 250) > 0) {
-		return;
-	}
+  if (Math.floor(Math.random() * 250) > 0) {
+    return;
+  }
 
-	if (message.author.bot) {
-		return;
-	}
+  if (message.author.bot) {
+    return;
+  }
 
-	const event =
-		quickTimeEvents[Math.floor(Math.random() * quickTimeEvents.length)];
-	event(message);
+  const event =
+    quickTimeEvents[Math.floor(Math.random() * quickTimeEvents.length)];
+  event(message);
 });
 
 client.on("interactionCreate", async (interaction) => {
-	if (botDisabled) {
-		if (interaction.isRepliable()) {
-			interaction.reply({
-				embeds: [disabledEmbed],
-			});
-		}
-		return;
-	}
+  if (botDisabled) {
+    if (interaction.isRepliable()) {
+      interaction.reply({
+        embeds: [disabledEmbed],
+      });
+    }
+    return;
+  }
 
-	if (interaction.isChatInputCommand()) {
-		try {
-			let userSelectResult = await db
-				.select()
-				.from(usersTable)
-				.where(eq(usersTable.userId, interaction.user.id));
-			if (userSelectResult.length < 1) {
-				userSelectResult = await db
-					.insert(usersTable)
-					.values({
-						userId: interaction.user.id,
-						balance: 0,
-					})
-					.returning();
-			}
+  if (interaction.isChatInputCommand()) {
+    try {
+      let userSelectResult = await db
+        .select()
+        .from(usersTable)
+        .where(eq(usersTable.userId, interaction.user.id));
+      if (userSelectResult.length < 1) {
+        userSelectResult = await db
+          .insert(usersTable)
+          .values({
+            userId: interaction.user.id,
+            balance: 0,
+          })
+          .returning();
+      }
 
-			const user = userSelectResult[0];
+      const user = userSelectResult[0];
 
-			commands[interaction.commandName].handler({ interaction, user, client });
-		} catch (e) {
-			console.error(e);
-			interaction.reply("Something went wrong...");
-		}
-	}
+      commands[interaction.commandName].handler({ interaction, user, client });
+    } catch (e) {
+      console.error(e);
+      interaction.reply("Something went wrong...");
+    }
+  }
 });
 
 client.on(Events.ClientReady, (e) => {
-	client.user?.setActivity({
-		name: "Life",
-		type: ActivityType.Competing,
-	});
-	console.info("Bot started");
+  client.user?.setActivity({
+    name: "Life",
+    type: ActivityType.Competing,
+  });
+  console.info("Bot started");
 });
 
 client.login(token);
 console.info("Bot started");
 
 process.on("uncaughtException", async (err, origin) => {
-	if (!client.isReady()) {
-		console.error("Client not ready when critical error occured.", origin, err);
-		return;
-	}
+  if (!client.isReady()) {
+    console.error(
+      "Client not ready when critical error occurred.",
+      origin,
+      err,
+    );
+    return;
+  }
 
-	const chan = await client.channels.fetch("1336093534880665684");
-	if (chan?.isSendable()) {
-		chan.send(
-			[
-				"Lifebot just thew a critical error. This log signifies a crash was prevented.",
-				`Origin: \`${origin}\``,
-				`Error: \`${err}\``,
-				`Stack: \`\`\`${err.stack}\`\`\``,
-			].join("\n"),
-		);
-	}
+  const chan = await client.channels.fetch("1336093534880665684");
+  if (chan?.isSendable()) {
+    chan.send(
+      [
+        "Lifebot just thew a critical error. This log signifies a crash was prevented.",
+        `Origin: \`${origin}\``,
+        `Error: \`${err}\``,
+        `Stack: \`\`\`${err.stack}\`\`\``,
+      ].join("\n"),
+    );
+  }
 });
