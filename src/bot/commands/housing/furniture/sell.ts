d3d3060c784f30e99b0a63b6a33930f0ceeb5cd8
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-03-12 20:02:54 -0400

    add try catch for sell database action

--- src/bot/commands/housing/furniture/sell.ts
+++ src/bot/commands/housing/furniture/sell.ts
@@ -1,9 +1,9 @@
 import {
-	ActionRowBuilder,
-	ButtonBuilder,
-	ButtonStyle,
-	ComponentBuilder,
-	EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ComponentBuilder,
+  EmbedBuilder,
 } from "discord.js";
 import { and, eq } from "drizzle-orm";
 import { db } from "../../../../db";
@@ -13,113 +13,129 @@ import type { LifebotCommandHandler } from "../../../types/commandTypes";
 import { Color } from "../../../utils/colors";
 import { nFormat } from "../../../utils/nFormat";
 import {
-	type FurnitureItem,
-	calculateFurniturePrice,
-	calculateFurnitureScore,
+  type FurnitureItem,
+  calculateFurniturePrice,
+  calculateFurnitureScore,
 } from "./furnitureCalculations";
 import { noFurnitureEmbed } from "./utils";
 
 export const sell: LifebotCommandHandler = async ({ interaction, user }) => {
-	const itemId = interaction.options.getInteger("id", true);
+  const itemId = interaction.options.getInteger("id", true);
 
-	const furnitureItem = await db
-		.select()
-		.from(furnitureTable)
-		.where(
-			and(
-				eq(furnitureTable.id, itemId),
-				eq(furnitureTable.ownerId, user.userId),
-			),
-		);
+  const furnitureItem = await db
+    .select()
+    .from(furnitureTable)
+    .where(
+      and(
+        eq(furnitureTable.id, itemId),
+        eq(furnitureTable.ownerId, user.userId),
+      ),
+    );
 
-	if (furnitureItem.length !== 1) {
-		interaction.reply({
-			embeds: [noFurnitureEmbed],
-		});
-		return;
-	}
+  if (furnitureItem.length !== 1) {
+    interaction.reply({
+      embeds: [noFurnitureEmbed],
+    });
+    return;
+  }
 
-	const realFurnitureItem = furnitureItem[0];
+  const realFurnitureItem = furnitureItem[0];
 
-	if (realFurnitureItem.houseId) {
-		interaction.reply({
-			embeds: [
-				new EmbedBuilder()
-					.setTitle("This is in use")
-					.setDescription("This furniture item is in use...")
-					.setColor(Color.RED),
-			],
-		});
-		return;
-	}
+  if (realFurnitureItem.houseId) {
+    interaction.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setTitle("This is in use")
+          .setDescription("This furniture item is in use...")
+          .setColor(Color.RED),
+      ],
+    });
+    return;
+  }
 
-	const sellValue = Math.floor(
-		calculateFurniturePrice(
-			calculateFurnitureScore(realFurnitureItem as FurnitureItem),
-			realFurnitureItem.originalValue,
-		),
-	);
+  const sellValue = Math.floor(
+    calculateFurniturePrice(
+      calculateFurnitureScore(realFurnitureItem as FurnitureItem),
+      realFurnitureItem.originalValue,
+    ),
+  );
 
-	const sellConfirmEmbed = new EmbedBuilder()
-		.setTitle("Are you sure?")
-		.setDescription(
-			`This is a very nice ${realFurnitureItem.material} ${realFurnitureItem.type}, but it is worth $${nFormat(sellValue)}`,
-		)
-		.setColor(Color.BLUE)
-		.setFooter({
-			text: "Expires in 60 seconds",
-		});
+  const sellConfirmEmbed = new EmbedBuilder()
+    .setTitle("Are you sure?")
+    .setDescription(
+      `This is a very nice ${realFurnitureItem.material} ${realFurnitureItem.type}, but it is worth $${nFormat(sellValue)}`,
+    )
+    .setColor(Color.BLUE)
+    .setFooter({
+      text: "Expires in 60 seconds",
+    });
 
-	const confirmComponent = new ActionRowBuilder<ButtonBuilder>().addComponents(
-		new ButtonBuilder()
-			.setLabel("Confirm")
-			.setCustomId("confirm")
-			.setStyle(ButtonStyle.Danger),
-	);
+  const confirmComponent = new ActionRowBuilder<ButtonBuilder>().addComponents(
+    new ButtonBuilder()
+      .setLabel("Confirm")
+      .setCustomId("confirm")
+      .setStyle(ButtonStyle.Danger),
+  );
 
-	const confirmMessage = await interaction.reply({
-		embeds: [sellConfirmEmbed],
-		components: [confirmComponent],
-	});
+  const confirmMessage = await interaction.reply({
+    embeds: [sellConfirmEmbed],
+    components: [confirmComponent],
+  });
 
-	confirmMessage
-		.awaitMessageComponent({
-			filter: (i) => i.user.id === user.userId,
-			time: 60_000,
-		})
-		.then(async (newI) => {
-			await db.transaction(async (t) => {
-				await t
-					.update(usersTable)
-					.set({
-						balance: increment(usersTable.balance, sellValue),
-					})
-					.where(eq(usersTable.userId, user.userId));
+  confirmMessage
+    .awaitMessageComponent({
+      filter: (i) => i.user.id === user.userId,
+      time: 60_000,
+    })
+    .then(async (newI) => {
+      try {
+        await db.transaction(async (t) => {
+          await t
+            .update(usersTable)
+            .set({
+              balance: increment(usersTable.balance, sellValue),
+            })
+            .where(eq(usersTable.userId, user.userId));
 
-				await t
-					.delete(furnitureTable)
-					.where(eq(furnitureTable.id, furnitureItem[0].id));
-			});
+          await t
+            .delete(furnitureTable)
+            .where(eq(furnitureTable.id, furnitureItem[0].id));
+        });
+      } catch (e) {
+        console.error(e);
 
-			const soldEmbed = new EmbedBuilder()
-				.setTitle("Item Sold!")
-				.setDescription(
-					`Sold a ${realFurnitureItem.material} ${realFurnitureItem.type} for $${nFormat(sellValue)}`,
-				)
-				.setColor(Color.BLUE);
+        newI.reply({
+          embeds: [
+            new EmbedBuilder()
+              .setTitle("Something went wrong")
+              .setDescription("Try again later.")
+              .setColor(Color.RED)
+              .setFooter({
+                text: "This error has been logged.",
+              }),
+          ],
+        });
+      }
 
-			await newI.reply({
-				embeds: [soldEmbed],
-			});
-		})
-		.catch(() => {
-			try {
-				confirmMessage.edit({
-					embeds: [sellConfirmEmbed.setColor(Color.RED).setTitle("Expired")],
-					components: [],
-				});
-			} catch (e) {
-				console.error(e);
-			}
-		});
+      const soldEmbed = new EmbedBuilder()
+        .setTitle("Item Sold!")
+        .setDescription(
+          `Sold a ${realFurnitureItem.material} ${realFurnitureItem.type} for $${nFormat(sellValue)}`,
+        )
+        .setColor(Color.BLUE);
+
+      await newI.reply({
+        embeds: [soldEmbed],
+      });
+    })
+    .catch(() => {
+      try {
+        confirmMessage.edit({
+          embeds: [sellConfirmEmbed.setColor(Color.RED).setTitle("Expired")],
+          components: [],
+        });
+      } catch (e) {
+        console.error(e);
+      }
+    });
 };
