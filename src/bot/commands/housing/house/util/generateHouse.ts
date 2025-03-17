9fd13a8e112da0095f90a12aedc2ccdbe2502902
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-03-17 13:18:57 -0400

    Don't generate quality or location of 0

--- src/bot/commands/housing/house/util/generateHouse.ts
+++ src/bot/commands/housing/house/util/generateHouse.ts
@@ -2,42 +2,42 @@ import { calculateHouseValue } from "./calculateHouseValue";
 import type { BasicHouseData } from "./types/houseType";
 
 const randomSquareFootage: () => number = () => {
-	const rand = Math.random();
-	// 1000 - 3000 80% chance
-	if (rand <= 0.8) {
-		return Math.floor(Math.random() * 2000 + 1000);
-	}
+  const rand = Math.random();
+  // 1000 - 3000 80% chance
+  if (rand <= 0.8) {
+    return Math.floor(Math.random() * 2000 + 1000);
+  }
 
-	// 3000 - 5,000 15% chance
-	if (rand <= 0.95) {
-		return Math.floor(Math.random() * 2000 + 3000);
-	}
+  // 3000 - 5,000 15% chance
+  if (rand <= 0.95) {
+    return Math.floor(Math.random() * 2000 + 3000);
+  }
 
-	// 5,000 - 10,000 3% chance
-	if (rand <= 0.99) {
-		return Math.floor(Math.random() * 5000 + 5000);
-	}
+  // 5,000 - 10,000 3% chance
+  if (rand <= 0.99) {
+    return Math.floor(Math.random() * 5000 + 5000);
+  }
 
-	//10,000 - 100,000 1%< chance
-	return Math.floor(Math.random() * 90_000 + 10_000);
+  //10,000 - 100,000 1%< chance
+  return Math.floor(Math.random() * 90_000 + 10_000);
 };
 
 export const generateHouse = () => {
-	const houseData: BasicHouseData = {
-		location: Math.floor(Math.random() * 101),
-		squareFootage: randomSquareFootage(),
-		quality: Math.floor(Math.random() * 101),
-		furnitureScore: 0,
-	};
+  const houseData: BasicHouseData = {
+    location: Math.floor(Math.random() * 100) + 1,
+    squareFootage: randomSquareFootage(),
+    quality: Math.floor(Math.random() * 100) + 1,
+    furnitureScore: 0,
+  };
 
-	/*
+  /*
     Ignore quality to prevent buying a house, renovating, and instantly selling for profit.
   */
 
-	return {
-		...houseData,
-		value: calculateHouseValue(houseData, {
-			ignoreQuality: true,
-		}),
-	};
+  return {
+    ...houseData,
+    value: calculateHouseValue(houseData, {
+      ignoreQuality: true,
+    }),
+  };
 };
